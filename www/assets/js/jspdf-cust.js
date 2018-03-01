aadae62ee0b1dbc37c906d58e4adff8f4e000b0c
jaredrlawson <jaredrlawson@gmail.com>
2018-03-01 15:36:24 -0500

    Add files via upload

--- www/assets/js/jspdf-cust.js
+++ www/assets/js/jspdf-cust.js
@@ -1,31 +1,45 @@
-output = function (type, options) {
-            var undef, data, length, array, i, blob;
-            switch (type) {
-            case undef:
-                return buildDocument();
-            case 'save':
-                if (navigator.getUserMedia) {
-                    if (window.URL === undefined) {
-                        return API.output('dataurlnewwindow');
-                    } else if (window.URL.createObjectURL === undefined) {
-                        return API.output('dataurlnewwindow');
-                    }
-                }
-                data = buildDocument();
-                write(data, options);                    
-                break;
-            case 'datauristring':
-            case 'dataurlstring':
-                return 'data:application/pdf;base64,' + btoa(buildDocument());
-            case 'datauri':
-            case 'dataurl':
-                document.location.href = 'data:application/pdf;base64,' + btoa(buildDocument());
-                break;
-            case 'dataurlnewwindow':
-                window.open('data:application/pdf;base64,' + btoa(buildDocument()));
-                break;
-            default:
-                throw new Error('Output type "' + type + '" is not supported.');
-            }
-            // @TODO: Add different output options
-        };
+    (function () {
+        var
+            form = $('#piInvoice_pdf'),
+            cache_width = form.width(),
+            a4 = [595.28, 841.89];  // for a4 size paper width and height
+        //alert(cache_width);
+        $('#create_pdf').on('click', function () {
+            //$('#create_pdf').hide();
+            $('body').scrollTop(0);
+            createPDF();
+            // $('#create_pdf').hide();
+        });
+        //create pdf
+        function createPDF() {
+            getCanvas().then(function (canvas) {
+            
+                //canvas.scale = 2;
+                //canvas.dpi = 144;
+                
+                var
+                    img = canvas.toDataURL("image/png"),
+                    doc = new jsPDF({
+                        unit: 'px',
+                        format: 'a4'
+                    });
+                doc.addImage(img, 'PNG', 24, 60);
+                doc.save('invoice1.pdf');
+                form.width(cache_width);
+                
+                
+                
+            });
+        }
+
+        // create canvas object
+        function getCanvas() {
+            form.width((a4[0] * 1.33333) - 80).css('max-width', 'none');
+            return html2canvas(form, {
+                imageTimeout: 2000,
+                removeContainer: true
+            });
+            
+        }
+
+    }());
